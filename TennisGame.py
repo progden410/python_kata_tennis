00203d0b30f8d0aa471cd30f4e9b7ead9bb5838f
progden <ccdrumer@gmail.com>
2024-08-01 00:57:28 +0800

    [test] 添加 TennisGame 單元測試
    * TennisGame 單元測試

---
+++ TennisGame.py
@@ -0,0 +1,7 @@
+class TennisGame:
+
+    def score(self, player):
+        pass
+
+    def score_string(self) -> str:
+        pass
--- testTennisGame.py
+++ testTennisGame.py
@@ -1,14 +1,90 @@
 import unittest
+
 from TennisGame import TennisGame
+
+PLAYER_1 = "Player 1"
+PLAYER_2 = "Player 2"
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_player1_score(self):
         # create a new TennisGame
         game = TennisGame()
+        self.assertEqual("love-all", game.score_string())
 
-        self.assertEqual("love-all", game.scoreString())
+        game.score(PLAYER_1)
+        self.assertEqual("15-love", game.score_string())
 
+        game.score(PLAYER_1)
+        self.assertEqual("30-love", game.score_string())
+
+        game.score(PLAYER_1)
+        self.assertEqual("40-love", game.score_string())
+
+        game.score(PLAYER_1)
+        self.assertEqual("Player 1 win.", game.score_string())
+
+    def test_player2_score(self):
+        # create a new TennisGame
+        game = TennisGame()
+        self.assertEqual("love-all", game.score_string())
+
+        game.score(PLAYER_2)
+        self.assertEqual("love-15", game.score_string())
+
+        game.score(PLAYER_2)
+        self.assertEqual("love-30", game.score_string())
+
+        game.score(PLAYER_2)
+        self.assertEqual("love-40", game.score_string())
+
+        game.score(PLAYER_2)
+        self.assertEqual("Player 2 win.", game.score_string())
+
+    def test_xxx_all(self):
+        # create a new TennisGame
+        game = TennisGame()
+        self.assertEqual("love-all", game.score_string())
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        self.assertEqual("15-all", game.score_string())
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        self.assertEqual("30-all", game.score_string())
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        self.assertEqual("40-all", game.score_string())
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        self.assertEqual("Deuce", game.score_string())
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        self.assertEqual("Deuce", game.score_string())
+
+    def test_advance(self):
+        # create a new TennisGame
+        game = TennisGame()
+        game.score(PLAYER_1)
+        game.score(PLAYER_1)
+        game.score(PLAYER_1)
+        game.score(PLAYER_2)
+        game.score(PLAYER_2)
+        game.score(PLAYER_2)
+        self.assertEqual("40-all", game.score_string())
+        game.score(PLAYER_1)
+        self.assertEqual("%s Advance." % PLAYER_1, game.score_string())
+        game.score(PLAYER_2)
+        self.assertEqual("Deuce", game.score_string())
+        game.score(PLAYER_1)
+        self.assertEqual("%s Advance." % PLAYER_1, game.score_string())
+        game.score(PLAYER_2)
+        self.assertEqual("Deuce", game.score_string())
+        game.score(PLAYER_2)
+        self.assertEqual("%s Advance." % PLAYER_2, game.score_string())
+        game.score(PLAYER_1)
+        self.assertEqual("Deuce", game.score_string())
+        game.score(PLAYER_2)
+        self.assertEqual("%s Advance." % PLAYER_2, game.score_string())
 
 if __name__ == '__main__':
     unittest.main()
